318175554363936964cfe56068fb9276e5703131
zblackwell <zachary.blackwell@gmail.com>
2025-05-21 15:13:23 -0600

    Added Clock and Delta Time

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     game_running = True
     while game_running:
@@ -16,6 +19,7 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
