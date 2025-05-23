e3be7d38fefa64a0e80216cae4c9cb86c2e5aeec
zblackwell <zachary.blackwell@gmail.com>
2025-05-23 15:17:02 -0600

    Generated classes; circle shape and player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -9,6 +10,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     game_running = True
@@ -18,6 +20,8 @@ def main():
                 return
         
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
