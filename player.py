3922162b75b9a986806622d4662f8791aa05b1bd
zblackwell <zachary.blackwell@gmail.com>
2025-05-25 13:35:36 -0600

    Added w/s key capture and player move

--- player.py
+++ player.py
@@ -21,6 +21,10 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt    
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -28,3 +32,10 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    
