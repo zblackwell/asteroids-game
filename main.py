5f4286279f682c9ff4d6488171fa4455ddf0f9dc
zblackwell <zachary.blackwell@gmail.com>
2025-05-25 13:48:07 -0600

    Added drawable and updateable groups and methods

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -20,8 +25,9 @@ def main():
                 return
         
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for obj in updatable:
+            obj.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60)/1000
