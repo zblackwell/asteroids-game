83af629d1d5148c027cfd5f48d965e4e851cd6d7
zblackwell <zachary.blackwell@gmail.com>
2025-05-25 18:01:20 -0600

    Added kill mechanichs and shooting cooldown

--- main.py
+++ main.py
@@ -49,10 +49,14 @@ def main():
             if player_collision:
                 print("Game Over!")
                 exit()
-
+            for shot in shots:
+                bullet_collision = obj.collision(shot)
+                if bullet_collision:
+                    obj.kill()
+                    shot.kill()
 
         pygame.display.flip()
-        dt = clock.tick(60)/1000
+        dt = clock.tick(60)/1000 
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,6 +28,8 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
+        if self.timer > 0:
+            self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -40,7 +43,9 @@ class Player(CircleShape):
             self.move(dt, -1)
 
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer <= 0:
+                self.shoot()
+            self.timer = PLAYER_SHOOT_COOLDOWN
 
     def shoot(self):
         bullet = Shot(self.position.x, self.position.y , self.rotation)
